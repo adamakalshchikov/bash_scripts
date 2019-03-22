ad6e69376aa442ca70ca10fa72c91ac742f90506
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-03-22 21:28:46 +0300

    Software loading and installing script

---
+++ soft_loading.sh
@@ -0,0 +1,19 @@
+# Скрипт служит для закгрузки ПО на установленную операционную систему
+# Использую apt, snap
+
+# В последующие строки следует вставлять команды, добавляющие репозитории
+sudo add-apt-repository -y ppa:atareao/flameshot
+
+# sudo apt --assume-yes
+sudo apt --assume-yes update
+
+
+sudo apt --assume-yes install remmina
+sudo apt --assume-yes install git
+sudo apt --assume-yes install pip3
+sudo apt --assume-yes install neovim
+sudo apt --assume-yes install flameshot
+
+sudo snap install pycharm-community
+sudo snap install telegram-desktop
+
