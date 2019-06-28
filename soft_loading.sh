2145065cbe9b8459ed0425953c934f287caf4b2a
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-06-29 00:09:29 +0300

    flameshot installation

--- soft_loading.sh
+++ soft_loading.sh
@@ -2,17 +2,18 @@
 # Использую apt, snap
 
 # В последующие строки следует вставлять команды, добавляющие репозитории
-sudo add-apt-repository -y ppa:atareao/flameshot
+sudo add-apt-repository ppa:atareao/flameshot
 
 # sudo apt --assume-yes
 sudo apt --assume-yes update
 
-
+sudo apt --assume-yes install flameshot
 sudo apt --assume-yes install remmina
 sudo apt --assume-yes install git
 sudo apt --assume-yes install pip3
 sudo apt --assume-yes install neovim
-sudo apt --assume-yes install flameshot
+sudo apt --assume-yes install redshift-gtk
+sudo apt --assume-yes install steam
 
 sudo snap install pycharm-community
 sudo snap install telegram-desktop
